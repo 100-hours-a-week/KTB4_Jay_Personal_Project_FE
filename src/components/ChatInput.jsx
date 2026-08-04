b5f915a618d3b0c27fb2eca57f8ae423ac45cbae
Jinhyeok Kim <jinxxang123@gmail.com>
2026-08-05 08:58:17 +0900

    수정 페이지 수정

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -27,7 +27,7 @@ function ChatInput({ disabled = false, errorMessage = '', onSend }) {
         <input
           className="chat-input"
           type="text"
-          placeholder={disabled ? '로그인 후 채팅에 참여할 수 있어요' : '채팅 메시지를 입력해주세요'}
+          placeholder={disabled ? '로그인 후 채팅에 참여할 수 있어요' : '  채팅 메시지를 입력해주세요'}
           value={message}
           disabled={disabled}
           onChange={(event) => setMessage(event.target.value)}
